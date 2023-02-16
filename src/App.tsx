9629fa8450e18c5d44f13135f676d7f75c8b3cec
Alin-Alexandru-Ion <ionalinalexandru@gmail.com>
2023-02-16 21:09:22 +0100

    Prepared project for Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,63 @@
 import React from 'react';
-//import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
+import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <nav className="nav-items">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" className='logo' />
+        <ul className="list">
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function MainContent() {
+  return (
+    <div>
+      <h1>First React Project</h1>
+      <ol>
+        <li>Node.js Server</li>
+        <li>Typescript</li>
+        <li>React</li>
+        <li>Material UI</li>
+      </ol>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p className="rights">© 2023 AAI Development. All rights reserved.</p>
+    </footer>
+  );
+}
+
+function ButtonTest() {
+  return (
+    <div className='button-group'>
+      <Button variant="contained">Log in</Button>
+      <Button variant="contained">Register</Button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>First React Project</h1>
-        <ul>
-          <li>Node.js Server</li>
-          <li>Typescript</li>
-          <li>React Bootstrap</li>
-        </ul>
+      <Header />
+      <MainContent />
+      <ButtonTest />
+      <Footer />
     </div>
   );
 }
