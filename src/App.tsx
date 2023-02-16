be1183984b7535c6f475e6bcb8a564e7d38e17e1
Alin-Alexandru-Ion <ionalinalexandru@gmail.com>
2023-02-16 12:24:01 +0100

    First try

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
-//import './App.css';
+import './App.css';
+
+function Header() {
+  return (
+    <div>
+      <nav className="nav-items">
+        <header>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" className="logo" />
+        </header>
+        <ul className="list">
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <p className="rights">©2023 AAI Development. All rights reserved.</p>
+      </footer>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>First React Project</h1>
-        <ul>
+        <ol>
           <li>Node.js Server</li>
           <li>Typescript</li>
-          <li>React Bootstrap</li>
-        </ul>
+          <li>React</li>
+          <li>Material UI</li>
+        </ol>
+      <Footer />
     </div>
   );
 }
