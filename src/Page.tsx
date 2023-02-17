369b98060740b34c17c9b7a6fe9249722861e880
Alin-Alexandru-Ion <ionalinalexandru@gmail.com>
2023-02-17 14:43:44 +0100

    Dwelved into basic Material UI components and theming

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import Button from '@mui/material/Button';
-
 import './Page.css';
 
 import Header from './Header';
@@ -13,21 +7,11 @@ import MainContent from './MainContent';
 import Footer from './Footer';
 
 
-function ButtonTest() {
-  return (
-    <div className='button-group'>
-      <Button variant="contained">Log in</Button>
-      <Button variant="contained">Register</Button>
-    </div>
-  );
-}
-
 function Page() {
   return (
     <div className="App">
       <Header />
       <MainContent />
-      <ButtonTest />
       <Footer />
     </div>
   );
---
+++ src/Themes/Theme.tsx
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+const BlueTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#5386F5',
+            contrastText: '#FFFFFF',
+        },
+        secondary: {
+            main: '#F5AD18',
+            contrastText: '#FFFFFF',
+        },
+    },
+});
+  
+export default BlueTheme;
